53c42e8a708e3e0331ff18f8bb8232ab2f2c7897
Zhenya <zhenyamalchenko0301@gmail.com>
2026-09-23 16:59:25 +0200

    Implement student A algorithm

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 #include "shared_types.h"
+#include <memory>
+unique_ptr<Result> calculateA(shared_ptr<const InputData> data);
 
 int main() {
-    cout << "Лаб 1, Мальченко/Слюсар" <<endl;
+    auto data = make_shared<InputData>(InputData{
+        [](double x) { return x * x; },
+        -2.0,
+        2.0,
+        0.0001
+    });
+
+    auto resultA = calculateA(data); //виклик функції
+    auto [x, y, iters, status] = *resultA; //результа за допомогою structured bindings
+
+    cout << "Метод золотого перерізу студента А\n";
+    cout << "Екстремум X: " << x << "\n";
+    cout << "Екстремум Y: " << y << "\n";
+    cout << "Ітерації: " << iters << "\n";
+    cout << "Статус коду: " << status << "\n";
+
     return 0;
 }
--- student_a.cpp
+++ student_a.cpp
@@ -1,6 +1,46 @@
 #include "shared_types.h"
+using namespace std;
+#include <memory>
+#include <cmath>
 
-Result calculateStudentA(const InputData& data) {
-    Result res = {0.0, 0.0, 0, -1};
+unique_ptr<Result> calculateA(shared_ptr<const InputData> data) {
+    auto res = make_unique<Result>();
+    
+    double a = data->a;
+    double b = data->b;
+    double eps = data->epsilon;
+    auto f = data->func;
+
+    const double phi = (1.0 + sqrt(5.0)) / 2.0;
+    const double resphi = 2.0 - phi;
+
+    double x1 = a + resphi * (b - a);
+    double x2 = b - resphi * (b - a);
+    double f1 = f(x1);
+    double f2 = f(x2);
+
+    int iter = 0;
+    while ((b - a) / 2.0 > eps) {
+        iter++;
+        if (f1 < f2) { 
+            b = x2;
+            x2 = x1;
+            f2 = f1;
+            x1 = a + resphi * (b - a);
+            f1 = f(x1);
+        } else {
+            a = x1;
+            x1 = x2;
+            f1 = f2;
+            x2 = b - resphi * (b - a);
+            f2 = f(x2);
+        }
+    }
+
+    res->extremum_x = (a + b) / 2.0;
+    res->extremum_y = f(res->extremum_x);
+    res->iterations = iter;
+    res->status_code = 0;
+
     return res;
 }
